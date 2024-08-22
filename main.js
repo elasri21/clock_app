5750ea0efca6563ab9299136e8b861f2dd3bb465
elasri21 <islame.edu@gmail.com>
2024-08-22 12:22:45 +0100

    add files

---
+++ main.js
@@ -0,0 +1,63 @@
+const moreBtn = document.querySelector(".more button");
+const moreSpan = document.querySelector(".more span");
+const moreDetails = document.querySelector(".bottom");
+const main = document.querySelector("main .top");
+const quot = document.querySelector(".quot blockquote");
+const author = document.querySelector(".quot .author h3");
+const refreshBtn = document.querySelector(".quot-coontainer button");
+const greedingContainer = document.querySelector(".greeding");
+moreBtn.addEventListener("click", function() {
+    this.querySelector("img").classList.toggle('rotate');
+    moreSpan.textContent = moreSpan.textContent == 'more' ? 'less' : 'more';
+    moreDetails.classList.toggle("bounce");
+    main.classList.toggle("bounce");
+    
+});
+
+const timeZone = document.querySelector(".city h3");
+const dayYear = document.querySelector(".days-year h3");
+const dayWeek = document.querySelector(".days-week h3");
+const week = document.querySelector(".week h3");
+
+const abbreviation = document.querySelector(".current-time small");
+const city = document.querySelector(".time-zoon .zone p");
+const currentTime = document.querySelector(".current-time h2");
+function getWorldTime() {
+    axios.get('https://worldtimeapi.org/api/ip').then((quotesRes) => {
+    const chosenQuote = quotesRes.data;
+    const date = new Date(chosenQuote['datetime']);
+    const hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+    const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+    const hour_min = `${hours}:${minutes}`;
+    greeting(hours);
+    timeZone.textContent = chosenQuote['timezone'];
+    dayYear.textContent = chosenQuote['day_of_year'];
+    dayWeek.textContent = chosenQuote['day_of_week'];
+    week.textContent = chosenQuote['week_number'];
+    abbreviation.textContent = chosenQuote['abbreviation'];
+    city.textContent = chosenQuote['timezone'];
+    currentTime.textContent = hour_min;
+    // console.log(quotesRes)
+    // console.log(chosenQuote)
+
+    }).catch((err) => console.error(err))
+}
+
+getWorldTime()
+refreshBtn.addEventListener("click", getWorldTime);
+
+function greeting(hour) {
+    if (hour < 12) {
+        greedingContainer.textContent = "Good Morning!";
+    }
+    else if (hour < 18) {
+        greedingContainer.textContent = "Good Afternoon!";
+    }
+    else {
+        greedingContainer.textContent = "Good Evening!";
+    }
+}
+
+setInterval(function() {
+    getWorldTime();
+}, 60000)
